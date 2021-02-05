c35c3733c74e8ca174da51b06b529d36928673b3
Do Tran Nhat Duy <napanreaper2506@gmail.com>
2021-02-05 13:07:50 +0700

    customize chat room

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
-
+import ChatFeed from './components/ChatFeed'
 function App() {
   return (
-    <ChatEngine height="100vh"
-      projectID=""
-      userName="" />
+    <ChatEngine
+      height="100vh"
+      projectID="b63a0db0-a3ed-4111-8d8c-0e708eef715f"
+      userName="tuyen"
+      userSecret="123"
+      renderChatFeed={(chatAppState) => <ChatFeed {...chatAppState} />} />
   );
 }
 
